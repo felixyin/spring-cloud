aeb61a69055226bfbe13c482068abebc74966a3b
尹彬 <1052921694@qq.com>
2020-12-25 21:25:51 +0800

    hystrix 服务降级 配置方式

--- cloud-provider-hystrix-payment8001/src/main/java/com/qtrj/springcloud/server/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/qtrj/springcloud/server/PaymentService.java
@@ -1,5 +1,7 @@
 package com.qtrj.springcloud.server;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -13,8 +15,14 @@ public class PaymentService {
         return "线程池：" + threadName + " paymentInfo_OK,id:" + id + "\t哈哈～～";
     }
 
+    //    服务降级
+    // 设置超时后的处理方法
+    @HystrixCommand(fallbackMethod = "paymentInfo_Timeout_handler", commandProperties = {
+            // 设置此方法允许的默认超时时间
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_Timeout() {
-        int tm = 3;
+        int tm = 5;
         try {
             TimeUnit.SECONDS.sleep(tm);
         } catch (InterruptedException e) {
@@ -22,6 +30,10 @@ public class PaymentService {
         }
         threadName = Thread.currentThread().getName();
         return "线程池：" + threadName + " paymentInfo_Timeout," + "\t哈哈～～\t耗时" + tm + "秒";
+    }
+
+    public String paymentInfo_Timeout_handler() {
+        return "线程池：" + threadName + " paymentInfo_Timeout_handler," + "\t😯～～\t超时了";
     }
 
 }
