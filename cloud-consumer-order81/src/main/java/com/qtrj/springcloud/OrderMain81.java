fd5df6ca58b1b8350a41a90aec3546d8e0a94767
尹彬 <1052921694@qq.com>
2020-12-25 13:18:01 +0800

    euraka 单机版配置

--- cloud-consumer-order81/src/main/java/com/qtrj/springcloud/OrderMain81.java
+++ cloud-consumer-order81/src/main/java/com/qtrj/springcloud/OrderMain81.java
@@ -2,8 +2,10 @@ package com.qtrj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain81 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain81.class);
