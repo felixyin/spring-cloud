2b89280ec66215ce35c82aadc86de2fb66de1989
尹彬 <1052921694@qq.com>
2020-12-25 19:50:44 +0800

    OpenFeign 服务接口封装restTemplate+Ribbon

---
+++ cloud-consumer-feign-order80/src/main/java/com/qtrj/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,16 @@
+package com.qtrj.springcloud.service;
+
+import com.qtrj.springcloud.entities.CommentResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient("CLOUD-PROVIDER-PAYMENT")
+public interface PaymentFeignService {
+
+    @GetMapping("/payment/get/{id}")
+    public CommentResult getPaymentById(@PathVariable("id") Long id);
+
+}
