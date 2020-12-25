101fcb274466e12ac4fcdddfde21c7b8f563246f
尹彬 <1052921694@qq.com>
2020-12-25 17:55:37 +0800

    自定义Ribbon自定义负载均衡算法

---
+++ cloud-consumer-order81/src/main/java/com/qtrj/myrule/MySelfRule.java
@@ -0,0 +1,18 @@
+package com.qtrj.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自定义Ribbon自定义负载均衡算法
+ */
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule myRule() {
+        return new RandomRule();
+    }
+}
--- cloud-consumer-order81/src/main/java/com/qtrj/springcloud/OrderMain81.java
+++ cloud-consumer-order81/src/main/java/com/qtrj/springcloud/OrderMain81.java
@@ -1,11 +1,14 @@
 package com.qtrj.springcloud;
 
+import com.qtrj.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PROVIDER-PAYMENT", configuration = MySelfRule.class)
 public class OrderMain81 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain81.class);
