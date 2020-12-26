71127a717ebbfdbcccc48d3e4ca08e365b20a40a
尹彬 <1052921694@qq.com>
2020-12-26 20:21:16 +0800

    stream入门

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/qtrj/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,24 @@
+package com.qtrj.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void inputMessage(Message<String> message) {
+        log.info("消费者1，" + message.getPayload() + "," + serverPort);
+    }
+
+}
