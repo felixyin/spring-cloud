43064c8041127a8efb4de939e1cef897849298b9
尹彬 <1052921694@qq.com>
2020-12-26 13:11:39 +0800

    gateway 自定义过滤器、断言

---
+++ cloud-gateway-gateway9527/src/test/java/ZonedPredicatesGenTEst.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import java.time.ZonedDateTime;
+
+public class ZonedPredicatesGenTEst {
+
+    @Test
+    public void testGetTimeStr() {
+
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
