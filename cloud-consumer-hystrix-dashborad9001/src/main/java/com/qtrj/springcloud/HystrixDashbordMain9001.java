fdc67bab898acb7c11631707cb83290f04488ee1
尹彬 <1052921694@qq.com>
2020-12-26 11:30:52 +0800

    hystrix监控面板

---
+++ cloud-consumer-hystrix-dashborad9001/src/main/java/com/qtrj/springcloud/HystrixDashbordMain9001.java
@@ -0,0 +1,21 @@
+package com.qtrj.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * http://localhost:9001/hystrix
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashbordMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashbordMain9001.class);
+    }
+
+}
