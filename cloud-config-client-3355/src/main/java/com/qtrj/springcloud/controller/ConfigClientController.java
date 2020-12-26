1991ae4e7504b02aa9fed50b7f24d2abac21d511
尹彬 <1052921694@qq.com>
2020-12-26 19:22:01 +0800

    test 自动消息总线自动刷新配置

--- cloud-config-client-3355/src/main/java/com/qtrj/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/qtrj/springcloud/controller/ConfigClientController.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigClientController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @Value("${config.info}")
     private String configInfo;
 
-//    http://localhost:3355/configInfo
     @GetMapping("/configInfo")
     public String getConfigInfo(){
-        return configInfo;
+        return "server.port = " + serverPort + "\t configInfo=" + configInfo;
     }
 }
